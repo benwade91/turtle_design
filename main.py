09500af74d8f80d4e9273df626e79596651d2d3b
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-11 13:46:23 -0700

    homemade hirst dot art

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+import colorgram
 import turtle
 import random
 
+colors = colorgram.extract('hirst.jpg', 10)
+rgb_list = []
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    if (r + g + b) < 740:
+        rgb_list.append((r, g, b))
+
 tim = turtle.Turtle()
-tim.shape("turtle")
-tim.pensize(2)
 tim.speed(0)
-colors = ["LightSteelBlue", "SkyBlue", "DarkCyan", "MediumSpringGreen", "Gold", "RosyBrown", "MediumVioletRed", "MediumPurple"]
-
 turtle.colormode(255)
-heading = 0
-for _ in range(100):
-    rand_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    tim.pencolor(rand_color)
-    tim.setheading(heading)
-    tim.circle(150)
-    heading += 20
+tim.penup()
+y = 225
+tim.goto(-225, y)
 
+for _ in range(9):
+    tim.dot(20, random.choice(rgb_list))
+    for _ in range(9):
+        tim.forward(50)
+        tim.dot(20, random.choice(rgb_list))
+    y -= 50
+    tim.goto(-225, y)
+
+print(rgb_list)
 screen = turtle.Screen()
+
 screen.exitonclick()
