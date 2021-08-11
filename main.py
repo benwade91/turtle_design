da97e6ef71d9da420533e0fda08caf864242d8b2
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-11 12:14:10 -0700

    random color for turtle path

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ tim.pensize(10)
 tim.speed(10)
 colors = ["LightSteelBlue", "SkyBlue", "DarkCyan", "MediumSpringGreen", "Gold", "RosyBrown", "MediumVioletRed", "MediumPurple"]
 
+screen = Screen()
+screen.colormode(255)
+
 for _ in range(100):
-    tim.pencolor(random.choice(colors))
-    tim.setheading(random.choice([0, 90, 180, 270]))
+    rand_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    tim.pencolor(rand_color)
+    tim.setheading(random.randint(0, 360))
     tim.forward(25)
 
 
-screen = Screen()
 
 screen.exitonclick()
