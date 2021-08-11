59410fc5cdbc024da427c0396520e51d5c3aa14c
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-11 10:18:47 -0700

    tim draws a square

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from turtle import Turtle, Screen
 
-tim_the_turtle = Turtle()
-tim_the_turtle.shape("turtle")
+tim = Turtle()
+tim.shape("turtle")
 
+tim.penup()
+tim.goto(-50, 50)
+tim.pendown()
 
-
-
+for _ in range(4):
+    tim.forward(100)
+    tim.rt(90)
 
 
 
