2cfbeda3565ac3a8b1911b28d315b3a63b1cec25
benwade91 <70988204+benn925@users.noreply.github.com>
2021-08-11 11:11:36 -0700

    tim draws a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, and decagon

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from turtle import Turtle, Screen
 tim = Turtle()
 tim.shape("turtle")
 tim.penup()
-tim.goto(-200, 0)
+tim.goto(-50, 150)
 tim.pendown()
+tim.pensize(5)
+colors = ["red", "green", "blue", "orange", "pink", "yellow", "black", "purple", "cyan", "blue", "green"]
 
-for _ in range(20):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
-
+for i in range(3, 11):
+    tim.pencolor(colors[i])
+    for _ in range(i):
+        tim.forward(100)
+        tim.right(360/i)
 
 
 screen = Screen()
